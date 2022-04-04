b2a61d5c352a6fb2f63f21578a232d0d79538d11
glamorrr <anugrahtoni23@gmail.com>
2022-04-04 16:48:30 +0700

    add landing pad detail page

--- modules/LandingPads/api.ts
+++ modules/LandingPads/api.ts
@@ -1,8 +1,12 @@
-import fetcher from '@/configs/fetcher'
-import {TLandpad} from './entities'
+import fetcher from "@/configs/fetcher";
+import { TLandpad } from "./entities";
 
-export const fetchAllLandPads = async ():Promise<TLandpad[]> => {
-    const res = await fetcher.get('/landpads')
-    return res.data
-}
+export const fetchAllLandPads = async (): Promise<TLandpad[]> => {
+  const res = await fetcher.get("/landpads");
+  return res.data;
+};
 
+export const fetchLandPad = async (id: string): Promise<TLandpad> => {
+  const res = await fetcher.get(`/landpads/${id}`);
+  return res.data;
+};
